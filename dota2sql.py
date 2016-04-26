b4a002c560a8834731be8adc2d43df9755840600
majinliang <18233698150@163.com>
2016-04-26 22:19:35 +0800

    完成set中的steamid和accountid

--- dota2sql.py
+++ dota2sql.py
@@ -485,3 +485,5 @@ if '__main__' == __name__:
     # print(json.dumps(Dota2SQL.get_match_details(1367828649)))
     # print(len(Dota2SQL.get_steam_msg(88)['players']))
     # test3()
+    print(Dota2SQL.get_match_history(account_id=170038624))
+    print("nihao")
